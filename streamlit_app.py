6f0c2059028d1717ceef6e68d15a06193d410913
AlexJ <alex_jorna@yahoo.com>
2025-05-30 13:02:34 -0600

    Update streamlit_app.py
    Adding ython library pandas and testing it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import pandas as pd
 import requests
 
 # Write directly to the app
@@ -17,6 +18,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop
 
+#convert the Snowpark Dataframe to a Pandas dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     , my_dataframe
@@ -29,7 +35,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
              ingredients_string += fruit_chosen + ' '
              st.subheader(fruit_chosen +' Nutrition Information')
-             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
+             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
              sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
  
 
